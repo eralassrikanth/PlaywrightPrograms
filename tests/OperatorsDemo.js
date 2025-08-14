23fd78ca5bcec10d3b228f925038976884af7c33
eralas2255 <srikantheralas@gmail.com>
2025-08-14 13:45:15 +1000

    first commit

---
+++ tests/OperatorsDemo.js
@@ -0,0 +1,10 @@
+// LogicalAndDemo.ts
+
+let age = 25;
+let hasLicense = true;
+
+if (age >= 18 && hasLicense) {
+    console.log("You are eligible to drive.");
+} else {
+    console.log("You are not eligible to drive.");
+}
